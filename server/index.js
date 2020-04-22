22c8934bb6edf8f58e72ab722d78da38f8c6b7b7
chanasueck <chanasuek.c@cdg.co.th>
2020-04-22 21:44:15 +0700

    add route post and delete

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 const dotenv = require('dotenv');
 const mwPublicRoute = require('./middleware/public-route')
 const mwCookie = require('./middleware/cookie')
-const { findAll, findById } = require('./controller/user')
+const { findAll, findById, addOneRecord, delManyRecord } = require('./controller/user')
+const { validateInsertUser, validateDeleteUser } = require('./utils/validate')
 dotenv.config();
 
 const port = process.env.PORT || 5000
@@ -19,12 +20,13 @@ const middleware = [
   mwCookie()
 ]
 
-
-
 app
   .use(middleware)
   .get('/api/user', findAll)
   .get('/api/user/:pid', findById)
+  .post('/api/user', validateInsertUser, addOneRecord)
+  .delete('/api/user', validateDeleteUser, delManyRecord)
+
 app.listen(port, () => console.log(`server online port ${port}`))
 
 module.exports = app
